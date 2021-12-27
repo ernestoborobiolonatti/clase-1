d85b5e6552f3c653f203499aad345fe6ac1f05ac
Ernesto Borobio <ernestoborobio@gmail.com>
2021-12-27 19:58:52 -0300

    clase 1 JS

---
+++ js/codigo.js
@@ -0,0 +1,3 @@
+let nombre = prompt("BIENVENIDO - Ingrese su nombre por favor");
+let apellido = prompt("Ahora ingrese su apellido");
+alert("Login correcto, Bienvenido " + nombre + " " + apellido + "...");
